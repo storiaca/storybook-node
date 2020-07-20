af1921c2b8e8afb2b6a85275c0d8dde38fae165a
Software/Nation <storiaca@gmail.com>
2020-07-20 16:37:08 +0200

    Login and dashboard layout

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 // @desc Login / Landing page
 // @route GET /
 router.get("/", (req, res) => {
-  res.send("Login");
+  res.render("login");
+});
+
+// @desc Dashboard
+// @route GET / dashboard
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
 
 module.exports = router;
